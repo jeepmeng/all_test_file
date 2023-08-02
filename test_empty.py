ccff14334656b89cb46bbf85b9e59f522cbaee03
jeepmeng <fucongliu0726@gmail.com>
2023-08-02 17:04:00 +0800

    flask

--- test_empty.py
+++ test_empty.py
@@ -1,4 +1,20 @@
-def test():
-    str_1 = ""
-    print(str_1)
-test("ll")
+import psutil
+import sched
+import time
+import os
+schedule_enter =sched.scheduler (time.time, time.sleep)
+
+def task():
+    print("run time: {}".format(int(time.time())))
+
+def recycle_eval(c, inc):
+    schedule_enter.enter( inc,0,recycle_eval,(c,inc))
+    # os.system(c)
+    task()
+
+if __name__ == "__main__":
+    inc = 3
+
+    recycle_eval(int(time.time()), inc)
+    schedule_enter.run()
+    # print("A:%2f MB"%(psutil.Process(os.getpid()).memory_info().rss/1024/1024))
